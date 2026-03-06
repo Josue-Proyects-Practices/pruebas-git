cca802844b37ef00f67479203d8145c75171d5ec
Carlos Raul Gregorio Abreu Teran <carlosuerbagt@hotmail.com>
2026-03-06 12:04:37 -0400

    se agrego la funcion Suma

--- index.js
+++ index.js
@@ -2,7 +2,14 @@
 
 const writeSomething = () => {
 
-    console.log("hola")
+    console.log("hola Carlos Abreu");
+
+    Suma(1, 2);
+
+
 }
+
+function Suma(a, b){
+    console.log(a + b);}
 
 writeSomething();
